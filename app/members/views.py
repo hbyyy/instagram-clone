8fbd1e0c6129ee54dc58181321bb3572f853324f
hbyyy <qjaduddl94@gmail.com>
2020-01-07 19:04:22 +0900

    숙제 내용 추가

--- app/members/views.py
+++ app/members/views.py
@@ -25,3 +25,25 @@ def login_view(request):
 
     else:
         return render(request, 'members/login.html')
+
+def signup_view(request):
+    """
+    Template: intex.html 사용
+                action만 이쪽으로
+    URL : /members/signup
+
+    User에 name필드를 추가
+        email
+        username
+        name
+        password
+    를 전달받아, 새로운 User를 생성한다,
+    생성 시, User.objects.create_user() 메서드를 사용한다.
+
+    이미 존재하는 username 또는 email을 입력한 경우,
+    "이미 사용중인 username/email입니다" 라는 메시지를 HttpResponse로 돌려준다.
+
+    생성에 성공하면 로그인 처리 후 (위의 login_view를 참조) posts:post-list로 redirection처리
+    :param request:
+    :return:
+    """
